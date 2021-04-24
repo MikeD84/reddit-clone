2f265bb44ce4fb1bce76509d1564953921e75a5d
MikeD84 <machtechinvest@gmail.com>
2021-04-24 22:55:26 +0200

    Adds all the code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import {Container, Flex, Spinner, VStack} from "@chakra-ui/core";
+import React, {useEffect, useState} from "react";
+import Post from "./components/post";
+import Navbar from "./components/navbar";
+import db from "./lib/firebase";
 
-function App() {
+
+const App = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    db.collection("posts")
+      .orderBy("createdAt", "desc")
+      .onSnapshot((querySnapshot) => {
+        const _posts = [];
+
+        querySnapshot.forEach((doc) => {
+          _posts.push({
+            id: doc.id,
+            ...doc.data(),
+          });
+        });
+
+        setPosts(_posts);
+      });
+  }, []);
+
+  useEffect(() => {
+    //hook to handle the initial fetcing of the posts
+    db.collection("posts")
+      .orderBy("createdAt", "desc")
+      .get()
+      .then((querySnapshot) => {
+        const data = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+
+        setPosts(data);
+      });
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Container maxW="md" centerContent p={8}>
+        <VStack spacing={8} w="100%">
+          {posts.map((post) => (
+            <Post post={post} key={posts.id} />
+          ))}
+        </VStack>
+      </Container>
+    </>
   );
 }
 
